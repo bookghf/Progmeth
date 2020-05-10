0c5c4896368d8f7b8d054cc6b445db650cb46044
bookky <bookghf@gmail.com>
2020-05-10 11:27:57 +0700

    Remove lab 3

--- 2110215_Lab_3_2019_2_toStudent/src/deck/InsertCardFailedException.java
+++
@@ -1,6 +0,0 @@
-package deck;
-
-//you CAN modify the first line
-public class InsertCardFailedException  {
-	// you CAN add SerialVersionID if eclipse gives you warning
-}
--- 2110215_Lab_3_2019_2_toStudent/src/deck/RemoveCardFailedException.java
+++
@@ -1,5 +0,0 @@
-package deck;
-
-public class RemoveCardFailedException extends Exception {
-	// you CAN add SerialVersionID if eclipse gives you warning
-}
